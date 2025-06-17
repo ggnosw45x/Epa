c9da8142ce6235e16cfe4c90dfcf0ebf3bb2d6ac
ggnosw45x <129354202+ggnosw45x@users.noreply.github.com>
2025-06-17 12:17:24 +0530

    Add files via upload

--- Plugins/Func.py
+++ Plugins/Func.py
@@ -1,5 +1,8 @@
 import sqlite3
+import threading
+
+db_lock = threading.Lock()
 
 def connect_to_db():
-    conn = sqlite3.connect('kurama.db')
+    conn = sqlite3.connect('kurama.db', check_same_thread=False)
     return conn
